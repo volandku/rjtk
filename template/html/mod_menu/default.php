403afbfc0920e34cfc2a3cf87cbf708bd2cd6935
Pazys <idm.box@gmail.com>
2013-02-04 10:40:09 +0400

    Пустой шаблончик
    Простенький шаблон
    Навбар - крошки - статьи - сайдбар правый и футер

---
+++ template/html/mod_menu/default.php
@@ -0,0 +1,67 @@
+<?php defined('_JEXEC') or die; ?>
+<ul class="nav menu <?php echo $class_sfx;?>"<?php
+	$tag = '';
+	if ($params->get('tag_id') != null)
+	{
+		$tag = $params->get('tag_id').'';
+		echo ' id="'.$tag.'"';
+	}
+?>>
+<?php
+foreach ($list as $i => &$item) :
+	$class = 'item-'.$item->id;
+	if ($item->id == $active_id) {
+		$class .= ' current';
+	}
+
+	if (in_array($item->id, $path)) {
+		$class .= ' active';
+	}
+	elseif ($item->type == 'alias') {
+		$aliasToId = $item->params->get('aliasoptions');
+		if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+			$class .= ' active';
+		}
+		elseif (in_array($aliasToId, $path)) {
+			$class .= ' alias-parent-active';
+		}
+	}
+
+	if ($item->deeper) {
+		$class .= ' deeper dropdown';
+	}
+
+	if ($item->parent) {
+		$class .= ' parent';
+	}
+
+	if (!empty($class)) {
+		$class = ' class="'.trim($class) .'"';
+	}
+
+	echo '<li'.$class.'>';
+
+	switch ($item->type) :
+		case 'separator':
+		case 'url':
+		case 'component':
+			require JModuleHelper::getLayoutPath('mod_menu', 'default_'.$item->type);
+			break;
+
+		default:
+			require JModuleHelper::getLayoutPath('mod_menu', 'default_url');
+			break;
+	endswitch;
+
+	if ($item->deeper) {
+		echo '<ul class="dropdown-menu">';
+	}
+	elseif ($item->shallower) {
+		echo '</li>';
+		echo str_repeat('</ul></li>', $item->level_diff);
+	}
+	else {
+		echo '</li>';
+	}
+endforeach;
+?></ul>
